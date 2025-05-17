8a3e4ec4cbe0129767c5dcc9cef2582e7e29749c
George Castro <gamcastro14@gmail.com>
2025-05-16 21:02:25 -0300

    Alterações efetuadas

--- calcula-saida.php
+++ calcula-saida.php
@@ -1,10 +1,3 @@
-<?php
-if (isset($_POST['Consultar'])) {
-    var_dump($_POST['hora_entrada']);
-    exit();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -20,31 +13,25 @@ if (isset($_POST['Consultar'])) {
 <body>
 
     <main class="main">
-        <div class="container">
-            <div>
-                <h1>Calculadora de tempo de saída</h1>
-                <p>Insira seu horário de check-in para calcular o horário de check-out</p>
-            </div>
-            <div>
-                <form method="post">
-                    <label for="hora_entrada">Hora de entrada: </label>
-                    <input type="text" name="hora_entrada">
-                    <p>Por favor use o formato 24h</p>
-                    <button type="submit" name="Consultar">Calcular</button>
-                </form>
-            </div>
-
-            <div>
-                <h2>Horário de Saída</h2>
-                <p>--:--</p>
-            </div>
-            <div>
-                <p>Notas:</p>
-                <p></p>
-            </div>
+        
+           
+                <div class="container">
+                    <h1>Calculadora de tempo de saída</h1>
+                    <p>Insira seu horário de check-in para calcular o horário de check-out</p>
+                    <form method="post">
+                        <label for="hora_entrada">Hora de entrada: </label>
+                        <input type="text" name="hora_entrada">
+                        <p>Por favor use o formato 24h</p>
+                        <button type="submit" name="Consultar">Calcular</button>
+                    </form>
+                    <h2>Horário de Saída</h2>
+                    <p>--:--</p>
+                    <p>Notas:</p>
+                    <p></p>
+                </div>           
 
 
-        </div>
+        
     </main>
     <footer>
 
